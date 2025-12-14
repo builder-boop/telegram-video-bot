6014fe41e317e68e75a7736312a1a9af9e502191
builder-boop <abilridho115@gmail.com>
2025-12-15 05:44:10 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,21 +3,21 @@
 # =========================
 
 # Token bot dari @BotFather
-BOT_TOKEN = "TOKEN_KAMU"
+BOT_TOKEN = "BOT_TOKEN"
 
 # ID Telegram owner (untuk akses unlimited & stats)
-OWNER_ID = 12345678  # ganti dengan ID kamu
+OWNER_ID =   # ganti dengan ID kamu
 
 # Folder untuk menyimpan file sementara
 DOWNLOAD_DIR = "downloads"
 
 # Limit harian
-LIMIT_PUBLIC = 10         # jumlah link yang bisa di-download public per hari
-LIMIT_UNLOCKED = 15      # jumlah link yang bisa di-download user unlocked per hari
+LIMIT_PUBLIC =          # jumlah link yang bisa di-download public per hari
+LIMIT_UNLOCKED =        # jumlah link yang bisa di-download user unlocked per hari
 
 # Resolusi maksimal
-MAX_PUBLIC_RES = 720     # max resolusi video untuk user public
-MAX_UNLOCKED_RES = 1080   # max resolusi video untuk user unlocked
+MAX_PUBLIC_RES =      # max resolusi video untuk user public
+MAX_UNLOCKED_RES =    # max resolusi video untuk user unlocked
 
 # =========================
 # END OF CONFIG
